16b216d6fff2bedbacc35e9ffed689acd149a820
kotime1 <thenewbryan@gmail.com>
2024-10-07 20:27:45 -0400

    very minor updates - still not working

--- test_setup.py
+++ test_setup.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 import os
 
 # Load environment variables
-load_dotenv()
-SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+load_dotenv("/home/kotime1/Documents/Projects/EchoScapes/py.env")
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 # OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
+print(f"SPOTIFY_CLIENT_ID: {SPOTIFY_CLIENT_ID}")
+print(f"SPOTIFY_CLIENT_SECRET: {SPOTIFY_CLIENT_SECRET}")
 # Spotify authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri="http://localhost:8000", scope="user-top-read"))
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri="http://localhost:8000", scope="user-top-read"))
 
-# Fetch user data
-user = sp.current_user()
-print(f"Logged in as {user['display_name']}")
+# # Fetch user data
+# user = sp.current_user()
+# print(f"Logged in as {user['display_name']}")
